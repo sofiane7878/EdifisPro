cedf34cbe24fd1f863ba6229f91df00df26cbece
Sofiane EL MHAMDI <elmhamdisofiane@gmail.com>
2025-08-22 05:50:41 +0200

    fix: Correction de la syntaxe PHP dans autoload_runtime.php

--- docker/start.sh
+++ docker/start.sh
@@ -5,7 +5,7 @@ sleep 10
 
 # Generer manuellement le fichier autoload_runtime.php
 if [ ! -f vendor/autoload_runtime.php ]; then
-    echo '<?php return require __DIR__./autoload.php;' > vendor/autoload_runtime.php
+    echo '<?php return require __DIR__ . /autoload.php;' > vendor/autoload_runtime.php
 fi
 
 # Nettoyer le cache
@@ -16,6 +16,7 @@ php bin/console assets:install public/ --relative
 
 # Executer les migrations (avec gestion d'erreur)
 php bin/console doctrine:migrations:migrate --env=prod --no-interaction || echo 'Migrations failed, continuing...'
+
 
 # Demarrer Apache
 apache2-foreground
